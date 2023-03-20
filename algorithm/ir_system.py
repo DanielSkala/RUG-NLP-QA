f1c2da8f32c7221ef457cfe7c6af5909ea9903ee
Tefeno <mo@tefeno.com>
2023-03-20 15:04:05 +0100

    added stuff

--- algorithm/ir_system.py
+++ algorithm/ir_system.py
@@ -1,27 +1,26 @@
 from abc import ABC
 from algorithm.caching_strategy import CachingStrategy
 from algorithm.answer_strategy import AnswerStrategy
-from algorithm.document_operator import DocumentOperator
 from algorithm.models import Document
 
 
 class IRSystem(ABC):
 
     def __init__(self,
-                 caching_strategy: CachingStrategy):
+                 caching_strategy: CachingStrategy,
+                 answer_strategy: AnswerStrategy):
         self.caching_strategy = caching_strategy
+        self.answer_strategy = answer_strategy
 
     def index_document(self, document: Document, *args, **kwargs):
-        parsed_obj = self.document_operator.parse(document.data, *args, **kwargs)
-        text_entries = self.caching_strategy.cache(parsed_obj, *args, **kwargs)
-        self.document_operator.store(text_entries, *args, **kwargs)
+        self.caching_strategy.cache(document)
 
-    def find(self, query: str, *args, **kwargs) -> str:
-        entries = self.caching_strategy.find(query, *args, **kwargs)
-        return self.answer_strategy.formulate_answer(query, entries, *args, **kwargs)
+    def find(self, doc_id: str, query: str, metadata: dict = None, *args, **kwargs) -> str:
+        entries = self.caching_strategy.find(doc_id, query, metadata)
+        return self.answer_strategy.formulate_answer(query, entries)
 
 
 class BookIRSystem(IRSystem):
 
-    def __init__(self, caching_strategy, answer_strategy, document_operator):
-        super().__init__(caching_strategy, answer_strategy, document_operator)
+    def __init__(self, caching_strategy, answer_strategy):
+        super().__init__(caching_strategy, answer_strategy)
