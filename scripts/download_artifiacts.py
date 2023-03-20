78e8caeda0f58ab9f93f1c34b998cbd05e1696fc
Tefeno <mo@tefeno.com>
2023-03-20 12:43:39 +0100

    added script to download models

---
+++ scripts/download_artifiacts.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+
+model_names = [
+    "distiluse-base-multilingual-cased-v1",
+]
+
+for model_name in model_names:
+    model = SentenceTransformer(model_name)
+    model.save(f"./artifacts/{model_name}")
