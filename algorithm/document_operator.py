70891986e1b979cdf527d998d17361465aac71de
Tefeno <mo@tefeno.com>
2023-03-20 14:01:12 +0100

    cleaning

--- algorithm/document_operator.py
+++ algorithm/document_operator.py
@@ -10,14 +10,6 @@ class DocumentOperator(ABC):
     def parse(self, document, *args, **kwargs) -> any:
         pass
 
-    @abstractmethod
-    def store(self, entries: [TextEntry], *args, **kwargs) -> bool:
-        pass
-
-    @abstractmethod
-    def retrieve(self, document_ids: [int], *args, **kwargs) -> [TextEntry]:
-        pass
-
 
 class PDFDocumentOperator(DocumentOperator):
     def parse(self, document, *args, **kwargs) -> any:
@@ -25,21 +17,3 @@ class PDFDocumentOperator(DocumentOperator):
         reader = PdfReader(data)
         text = [page.extract_text() for page in tqdm(reader.pages)]
         return text
-
-    def store(self, entries: [TextEntry], *args, **kwargs) -> bool:
-        try:
-            with open("entries.pkl", "wb") as f:
-                pickle.dump(entries, f)
-            return True
-        except Exception as e:
-            print(e)
-            return False
-
-    def retrieve(self, document_ids: [int], *args, **kwargs) -> [TextEntry]:
-        try:
-            with open("entries.pkl", "rb") as f:
-                entries = pickle.load(f)
-            return entries
-        except Exception as e:
-            print(e)
-            return None
