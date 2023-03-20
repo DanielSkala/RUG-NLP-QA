7e3918ba4cb22a3f849739634b97d016dccce11d
Danko Skala <dskala@slido.com>
2023-03-20 10:14:15 +0100

    implement basic document operator
    (update requirements.txt)

--- algorithm/document_operator.py
+++ algorithm/document_operator.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
-from algorithm.models import Document, TextEntry
+from algorithm.models import TextEntry
+from PyPDF2 import PdfReader
+from tqdm import tqdm
+import pickle
 
 
 class DocumentOperator(ABC):
@@ -19,9 +22,24 @@ class DocumentOperator(ABC):
 class PDFDocumentOperator(DocumentOperator):
     def parse(self, document, *args, **kwargs) -> any:
         data = document.data
+        reader = PdfReader(data)
+        text = [page.extract_text() for page in tqdm(reader.pages)]
+        return text
 
     def store(self, entries: [TextEntry], *args, **kwargs) -> bool:
-        pass
+        try:
+            with open("entries.pkl", "wb") as f:
+                pickle.dump(entries, f)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def retrieve(self, document_ids: [int], *args, **kwargs) -> [TextEntry]:
-        pass
+        try:
+            with open("entries.pkl", "rb") as f:
+                entries = pickle.load(f)
+            return entries
+        except Exception as e:
+            print(e)
+            return None
